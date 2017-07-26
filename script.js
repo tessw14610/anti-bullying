b280482800cc34719478fa07b296e11a5bf00831
tessw14610 <tessw14610@gmail.com>
2017-07-26 15:37:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function function1() {
+var x = document.getElementById("input").value;
+document.getElementById("").innerHTML = x;
+var x = document.getElementById("links");
+x.style.display = 'block';
+}
